217adf4f664c3f5cfd11f1c12a7da380ec404aab
Kevin Murray <spam@kdmurray.id.au>
2014-06-22 18:33:49 +1000

    [trieBCD] latest WIP. some parts still TODO
    - Finish load_barcodes
    - Implement main loop

--- src/trieBCD.c
+++ src/trieBCD.c
@@ -18,13 +18,39 @@
 
 #include "trieBCD.h"
 
+struct tbd_barcode *
+tbd_barcode_create(const char *seq, size_t len)
+{
+    struct tbd_barcode *bcd = NULL;
+
+    if (seq == NULL || len < 1) {
+        return NULL;
+    }
+    bcd = km_malloc(sizeof(*bcd));
+    bcd->seq = strndup(seq, len+1);
+    if (bcd->seq == NULL) {
+        km_free(bcd);
+        return NULL;
+    }
+    bcd->len = len;
+    bcd->count = 0;
+    return bcd;
+}
+
+void
+tbd_barcode_destroy_(struct tbd_barcode *barcode)
+{
+    if (!tbd_barcode_ok(barcode)) return;
+    km_free(barcode->seq);
+    barcode->len = 0;
+    km_free(barcode);
+}
+
 struct tbd_config *
 tbd_config_create(void)
 {
     struct tbd_config *config = km_calloc(1, sizeof(*config));
-
     /* km_calloc never returns null, we use errprintexit as the err handler */
-    config->ok = 0;
     return config;
 }
 
@@ -46,14 +72,59 @@ tbd_config_destroy_(struct tbd_config *config)
     km_free(config->out_prefixes[1]);
     km_free(config->infiles[0]);
     km_free(config->infiles[1]);
-    for (iii = 0; iii < config->n_outputs_1; iii ++) {
-        for (jjj = 0; jjj < config->n_outputs_2; jjj++) {
+    for (iii = 0; iii < config->n_barcodes_1; iii ++) {
+        for (jjj = 0; jjj < config->n_barcodes_2; jjj++) {
             tbd_output_destroy(config->outputs[iii][jjj]);
+            tbd_barcode_destroy(config->barcodes[iii][jjj]);
         }
     }
     tbd_output_destroy(config->unknown_output);
 }
 
+
+static char *
+_tbd_format_outfile_path (const char *prefix, const char *id, int read,
+        const char *ext)
+{
+    char buf[4096];
+    int res = 0;
+
+    if (prefix == NULL || id == NULL) {
+        return NULL;
+    }
+    res = snprintf(buf, 4096, "%s_%s_R%d.%s", prefix, id, read, ext);
+    if (res >= 4096) {
+        return NULL;
+    }
+    return strndup(buf, 4096);
+}
+
+struct tbd_output *
+tbd_output_create(const char *fwd_fpath, const char *rev_fpath,
+        enum read_mode mode, const char *fp_mode)
+{
+    struct tbd_output *out = NULL;
+
+    if (mode == READS_UNKNOWN || fwd_fpath == NULL || \
+            (mode == READS_PAIRED && rev_fpath == NULL)) {
+        return NULL;
+    }
+    out = km_calloc(1, sizeof(*out));
+    out->mode = mode;
+    out->fwd_file = seqfile_create(fwd_fpath, fp_mode);
+    if (out->fwd_file == NULL) {
+        km_free(out);
+        return NULL;
+    }
+    out->rev_file = seqfile_create(rev_fpath, fp_mode);
+    if (out->rev_file == NULL) {
+        seqfile_destroy(out->fwd_file);
+        km_free(out);
+        return NULL;
+    }
+    out->count = 0;
+    return out;
+}
 
 void
 tbd_output_destroy_(struct tbd_output *output)
@@ -66,6 +137,52 @@ tbd_output_destroy_(struct tbd_output *output)
     }
 }
 
+int
+tbd_load_barcodes(struct tbd_config *config)
+{
+    intptr_t barcode_idx = 0;
+    size_t iii = 0;
+    int retval = -1;
+
+    if (!tbd_config_ok(config) || !config->have_cli_opts) {
+        return -1;
+    }
+    /* Create and alloc tries */
+    config->tries = km_malloc(config->mismatches * sizeof(*config->tries));
+    for (iii = 0; iii < config->mismatches; iii++) {
+        config->tries[iii] = tbd_trie_create();
+        if (config->tries[iii] == NULL) {
+            km_free(config->tries);
+            return 1;
+        }
+    }
+    seqfile_t *bcd_sf = seqfile_create(config->barcode_file, "r");
+    SEQFILE_ITER_SINGLE_BEGIN(bcd_sf, this_bcd, bcd_ln)
+        char **mutated = NULL;
+        size_t num_mutated = 0;
+        size_t jjj = 0;
+        int ret = 0;
+
+        /* Make mutated barcodes and add to trie */
+        for (iii = 0; iii < config->mismatches; iii++) {
+            mutated = hamming_mutate_dna(&num_mutated, this_bcd->seq.s,
+                                         this_bcd->seq.l, iii, 0);
+            for (jjj = 0; jjj < num_mutated; jjj++) {
+                ret = tbd_trie_add(config->tries[iii], mutated[jjj],
+                             barcode_idx);
+                if (ret != 0) {
+                    printf("Big fuckup about here\n"); /* TODO */
+                    retval = 1;
+                    break;
+                }
+            }
+        }
+        /* Open barcode files */
+        /* TODO */
+        barcode_idx++;
+    SEQFILE_ITER_SINGLE_END(this_bcd)
+    return retval;
+}
 
 int
 combinations(uint64_t len, uint64_t elem, uintptr_t *choices)
@@ -152,7 +269,6 @@ product(uint64_t len, uint64_t elem, uintptr_t *choices, int at_start)
     return 0;
 }
 
-
 char **
 hamming_mutate_dna(size_t *n_results_o, const char *str, size_t len,
         int dist, int keep_original)
@@ -207,3 +323,92 @@ hamming_mutate_dna(size_t *n_results_o, const char *str, size_t len,
     return result;
 }
 
+struct tbd_trie *
+tbd_trie_create(void)
+{
+    struct tbd_trie *trie = NULL;
+    AlphaMap *map = alpha_map_new();
+    int ret = 0;
+
+    /* Make trie AlphaMap */
+    if (map == NULL) {
+        return NULL;
+    }
+    #define _AM_ADD(chr)                                                        \
+    ret = alpha_map_add_range(map, chr, chr);                               \
+    if (ret == 0) {                                                         \
+        alpha_map_free(map);                                                \
+        return NULL;                                                        \
+    }
+    _AM_ADD('A')
+    _AM_ADD('C')
+    _AM_ADD('G')
+    _AM_ADD('T')
+    #undef _AM_ADD
+    trie = km_calloc(1, sizeof(*trie));
+    trie->trie = trie_new(map);
+    if (trie->trie == NULL) {
+        km_free(trie);
+        alpha_map_free(map);
+        return NULL;
+    }
+    return trie;
+}
+
+int
+tbd_trie_add (struct tbd_trie *trie, const char *seq, uintptr_t data)
+{
+    if (!tbd_trie_ok(trie)) return -1;
+    if (trie_store_if_absent(trie->trie, seq, data)) {
+        return 0;
+    }
+    return 1;
+}
+
+int
+tbd_barcode_match (intptr_t *value, struct tbd_trie *trie, const seq_t *seq)
+{
+    TrieState *trie_iter = NULL;
+    size_t seq_pos = 0;
+    size_t max_match_len = 0;
+    int res = 0;
+    intptr_t our_val = -1;
+
+    /* *value is set to -1 on anything bad happening including failed lookup */
+    if (value == NULL || !tbd_trie_ok(trie) || !seq_ok(seq)) {
+        *value = -1;
+        return -1;
+    }
+    if (seq->seq.l < trie->min_len) {
+        *value = -1;
+        return 1;
+    }
+    /* Only look until the maximum of the largest barcode, or seq len */
+    max_match_len = seq->seq.l < trie->max_len ? trie->max_len : seq->seq.l;
+    /* Grab tree root iter, and check it. */
+    trie_iter = trie_root(trie->trie);
+    if (trie_iter == NULL) {
+        *value = -1;
+        return -1;
+    }
+    /* Consume seq until we can't */
+    for (seq_pos = 0; seq_pos < max_match_len; seq_pos++) {
+        res = trie_state_walk(trie_iter, seq->seq.s[seq_pos]);
+        if (!res) {
+            break;
+        }
+    }
+    if (trie_state_is_terminal(trie_iter)) {
+        our_val = trie_state_get_data(trie_iter);
+    } else {
+        goto end;
+    }
+end:
+    trie_state_free(trie_iter);
+    *value = our_val;
+    if (our_val != -1) {
+        /* we've succeeded */
+        return 0;
+    }
+    return 1;
+}
--- src/trieBCD.h
+++ src/trieBCD.h
@@ -25,6 +25,17 @@
 #include <kmseq.h>
 #include <kmseqfile.h>
 
+#include "datrie/trie.h"
+#include "datrie/alpha-map.h"
+
+/* General rules:
+ *  Most functions are declared as `int X(...);`. These functions return:
+ *   -1 on parameter error (NULLs, bad values etc)
+ *    0 on success
+ *    1 on failure
+ *  Thus, one can check success with ret = X(...); if (ret != 0) {panic();}
+ */
+
 enum read_mode {
     READS_UNKNOWN = 0,
     READS_SINGLE = 1,
@@ -40,8 +51,16 @@ struct tbd_output {
 };
 
 struct tbd_trie {
-    void *trie;
+    Trie *trie; /* From datrie */
     int mismatch_level;
+    size_t max_len;
+    size_t min_len;
+};
+
+struct tbd_barcode {
+    char *seq;
+    size_t len;
+    uint64_t count;
 };
 
 struct tbd_config {
@@ -50,8 +69,9 @@ struct tbd_config {
     char *out_prefixes[2];
     /* Array of output files. Access by cfg->ofs[1st_bcd_idx][2nd_bcd_idx] */
     struct tbd_output ***outputs;
-    size_t n_outputs_1; /* Number of first read barcodes */
-    size_t n_outputs_2; /* Number of second read barcodes */
+    struct tbd_barcode ***barcodes;
+    size_t n_barcodes_1; /* Number of first read barcodes */
+    size_t n_barcodes_2; /* Number of second read barcodes */
     struct tbd_output *unknown_output; /* output for unknown files */
     enum read_mode in_mode;
     enum read_mode out_mode;
@@ -59,11 +79,56 @@ struct tbd_config {
     int mismatches;
     struct tbd_trie **tries;
     size_t n_tries;
-    int ok                      :1;
+    int have_cli_opts           :1;
     int name_outfiles_by_seq    :1;
 };
 
+char **hamming_mutate_dna(size_t *n_results_o, const char *str, size_t len,
+        int dist, int keep_original);
 
+static inline int
+tbd_config_ok(const struct tbd_config *config)
+{
+    if (config == NULL) return 0;
+    return 1;
+}
+
+static inline int
+tbd_trie_ok(const struct tbd_trie *trie)
+{
+    if (trie == NULL) return 0;
+    if (trie->trie == NULL) return 0;
+    if (trie->min_len > trie->max_len) return 0;
+    return 1;
+}
+
+static inline int
+tbd_barcode_ok(const struct tbd_barcode *barcode)
+{
+    if (barcode == NULL) return 0;
+    if (barcode->seq == NULL || barcode->len == 0) return 0;
+    return 1;
+}
+
+static inline int
+tbd_output_ok(const struct tbd_output *output)
+{
+    if (output == NULL) return 0;
+    if (output->mode == READS_UNKNOWN) return 0;
+    if (output->fwd_file == NULL) return 0;
+    if (output->mode == READS_PAIRED && output->rev_file == NULL) return 0;
+    return 1;
+}
+
+
+/*===  FUNCTION  ============================================================*
+Name:           tbd_config_create
+Paramters:      void
+Description:    Create a struct tbd_config on the heap and initialise members
+                to empty/null values.
+Returns:        struct tbd_config *: A valid, empty struct tbd_config, or NULL
+                on any error.
+ *===========================================================================*/
 struct tbd_config *tbd_config_create(void);
 
 /*===  FUNCTION  ============================================================*
@@ -81,12 +146,18 @@ void tbd_config_destroy_(struct tbd_config *config);
 
 
 /*===  FUNCTION  ============================================================*
-  Name:           tbd_output_create
-  Paramters:      struct tbd_config *config:
-  Description:    Opens the file.
-  Returns:        struct tbd_output *:
+Name:           tbd_output_create
+Paramters:      const char *fwd_fpath: Forwards/interleaved read filepath
+                const char *rev_fpath: Reverse read filepath
+                enum read_mode mode: Output mode
+                const char *fp_mode: zfopen() mode specifier. See
+                    /usr/include/zlib.h for valid values.
+Description:    Creates and opens file members of a struct tbd_output
+Returns:        struct tbd_output *: A valid struct tbd_output, or NULL on
+                failure of any kind
  *===========================================================================*/
-struct tbd_output *tbd_output_create(struct tbd_config *config);
+struct tbd_output *tbd_output_create(const char *fwd_fpath,
+        const char *rev_fpath, enum read_mode mode, const char *fp_mode);
 
 /*===  FUNCTION  ============================================================*
 Name:           tbd_output_destroy
@@ -101,7 +172,8 @@ void tbd_output_destroy_(struct tbd_output *output);
     out = NULL;                                                             \
     STMT_END
 
-int tbd_trie_create(int mismatch_level);
+
+struct tbd_trie *tbd_trie_create(void);
 
 /*===  FUNCTION  ============================================================*
 Name:           tbd_trie_destroy
@@ -116,9 +188,17 @@ void tbd_trie_destroy_(struct tbd_trie *trie);
     trie = NULL;                                                            \
     STMT_END
 
+struct tbd_barcode *tbd_barcode_create(const char *seq, size_t len);
+void tbd_barcode_destroy_(struct tbd_barcode *barcode);
+#define tbd_barcode_destroy(barcode) STMT_BEGIN                             \
+    tbd_barcode_destroy_(barcode);                                          \
+    barcode = NULL;                                                         \
+    STMT_END
 
-char **hamming_mutate_dna(size_t *n_results_o, const char *str, size_t len,
-        int dist, int keep_original);
+
+int tbd_load_barcodes(struct tbd_config *config);
+int tbd_trie_add(struct tbd_trie *trie, const char *seq, size_t len);
+
 int product(uint64_t len, uint64_t elem, uintptr_t *choices, int at_start);
 int combinations(uint64_t len, uint64_t elem, uintptr_t *choices);
 
