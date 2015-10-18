d981d6af1aea79db16202f3ef5b98bc68c930d59
Kevin Murray <spam@kdmurray.id.au>
2015-10-18 17:32:31 +1100

    Add proper copyright/license headers to source.

--- src/main.c
+++ src/main.c
@@ -2,16 +2,22 @@
  * ============================================================================
  *
  *       Filename:  axe_main.c
- *
  *    Description:  Main loop for axe
+ *      Copyright:  2014-2015 Kevin Murray <spam@kdmurray.id.au>
+ *        License:  GNU GPL v3+
  *
- *        Version:  1.0
- *        Created:  11/06/14 13:37:00
- *       Revision:  none
- *        License:  GPLv3+
- *       Compiler:  gcc, clang
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
  *
- *         Author:  Kevin Murray, spam@kdmurray.id.au
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * ============================================================================
  */
